3338adb1f5b441ed633358385cd508001a3bb9e6
Hattori <crafter.alloy@gmail.com>
2024-06-05 21:11:50 +0900

    壁に触れている判定

--- script.js
+++ script.js
@@ -92,13 +92,13 @@ class Random {
     this.z = 521288629;
     this.w = seed;
   }
-  
+
   // XorShift
   next() {
     let t;
     t = this.x ^ (this.x << 11);
     this.x = this.y; this.y = this.z; this.z = this.w;
-    return this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8)); 
+    return this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8));
   }
   next_int(min = 0, max = 1) {
     const r = Math.abs(this.next());
@@ -124,20 +124,10 @@ class Player {
     this.motion = args["motion"];
     this.attribute = args["attribute"];
     this.on_ground = args["on_ground"];
-    this.touching_left_wall = args["touching_left_wall"];
-    this.touching_right_wall = args["touching_left_wall"];
+    this.touching_wall = args["touching_wall"];
   }
 }
-class EntityAttribute {
-  constructor(attribute) {
-    this.size = attribute["size"];
-    this.move_speed = attribute["move_speed"];
-    this.gravity_multiplier = attribute["gravity_multiplier"];
-    this.resistance = attribute["resistance"];
-    this.wall_resistance = attribute["wall_resistance"];
-    this.jump_power = attribute["jump_power"];
-  }
-}
+
 // key input
 var input_left = false;
 var input_right = false;
@@ -148,7 +138,7 @@ document.addEventListener('keydown', event => {
   switch (event.key) {
     case "ArrowLeft": input_left = true; break;
     case "ArrowRight": input_right = true; break;
-    case "ArrowUp": input_up = true;break;
+    case "ArrowUp": input_up = true; break;
     case "ArrowDown": input_down = true; break;
     case "Space": input_space = true; break;
   }
@@ -157,7 +147,7 @@ document.addEventListener('keyup', event => {
   switch (event.key) {
     case "ArrowLeft": input_left = false; break;
     case "ArrowRight": input_right = false; break;
-    case "ArrowUp": input_up = false;start_input_up = false; break;
+    case "ArrowUp": input_up = false; start_input_up = false; break;
     case "ArrowDown": input_down = false; break;
     case "Space": input_space = false; break;
   }
@@ -171,48 +161,51 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
 // setting player
-const player_attribute = new EntityAttribute({
+player_attribute = {
   size: new Vector2(50, 50),
   move_speed: 1,
   gravity_multiplier: 1,
   jump_power: 20,
   resistance: 0.9,
   wall_resistance: 0.6,
-});
+};
 
 const p = new Player({
   pos: new Vector2(100, 100),
   motion: Vector2.zero,
   attribute: player_attribute,
   on_ground: false,
-  touching_left_wall: false,
-  touching_right_wall: false
+  touching_wall: { up: false, down: false, right: false, left: false }
 });
 
 // setting camera
 const camera = new Camera({
-  pos: new Vector2(0,0),
+  pos: new Vector2(0, 0),
   focus_object: p,
-  offset: new Vector2(400,300),
-  smoothness: 5
+  offset: new Vector2(400, 300),
+  smoothness: 10
 });
 
 // setting terrain
 var walls = [
-  [0, 0, 800, 50,"grass"],
-  [0, 590, 800, 10,"dirt"],
-  [0, 0, 10, 600,"dirt"],
-  [790, 0, 10, 600,"dirt"],
+  { rect: [0, 0, 800, 50], material: "grass" },
+  { rect: [0, 590, 800, 10], material: "dirt" },
+  { rect: [0, 0, 10, 600], material: "dirt" },
+  { rect: [790, 0, 10, 600], material: "dirt" },
+  { rect: [100, 150, 100, 50], material: "grass" },
+  { rect: [350, 150, 100, 50], material: "grass" },
+  { rect: [225, 300, 100, 50], material: "grass" },
+  { rect: [600, 200, 100, 50], material: "grass", animation: f => { return new Vector2(0, Math.sin(f / 50) * 50) } },
+]
 
-  [100, 150, 100, 50,"grass"],
-  [350, 150, 100, 50,"grass"],
-  [600, 150, 100, 50,"grass"],
-  [225, 300, 100, 50,"grass"],
-  [475, 300, 100, 50,"grass"]]
 
+// tick
+var game_frame = 0;
 function tick() {
   logic();
   render();
+  game_frame++;
+  game_frame %= 1000000;
 }
 
 function logic() {
@@ -228,19 +221,22 @@ function logic() {
   }
   if (input_down) { p.motion.y -= p.attribute.move_speed; }
 
+  // calculation
   p.motion.x *= p.attribute.resistance;
   p.motion.y += GRAVITY * p.attribute.gravity_multiplier;
   if (Math.abs(p.motion.x) < 0.1) { p.motion.x = 0 }
-  //if (Math.abs(p.motion.y) < 1) {p.motion.y = 0}
+  move();
 
-  move(4);
+  // touching and sinking
+  if (is_colliding(p.pos.x, p.pos.y + 1)) { p.touching_wall.up = true; } else { p.touching_wall.up = false; }
+  if (is_colliding(p.pos.x, p.pos.y - 1)) { p.touching_wall.down = true; } else { p.touching_wall.down = false; }
+  if (is_colliding(p.pos.x + 1, p.pos.y)) { p.touching_wall.right = true; } else { p.touching_wall.right = false; }
+  if (is_colliding(p.pos.x - 1, p.pos.y)) { p.touching_wall.left = true; } else { p.touching_wall.left = false; }
 }
 
-function move(accuracy) {
+function move() {
   if (!is_colliding(p.pos.x + p.motion.x, p.pos.y)) {
     p.pos.x += p.motion.x;
-    p.touching_left_wall = false;
-    p.touching_right_wall = false;
   } else {
     let length = Math.floor(Math.abs(p.motion.x)) + 1;
     for (let i = 1; i <= length; i++) {
@@ -248,14 +244,12 @@ function move(accuracy) {
         if (is_colliding(p.pos.x - i, p.pos.y)) {
           p.pos.x -= i;
           p.pos.x++;
-          p.touching_left_wall = true;
           break;
         }
       } else {
         if (is_colliding(p.pos.x + i, p.pos.y)) {
           p.pos.x += i;
           p.pos.x--;
-          p.touching_right_wall = true;
           break;
         }
       }
@@ -301,10 +295,10 @@ function is_colliding(px, py) {
       py + p.attribute.size.y,
       px + p.attribute.size.x / 2,
       py,
-      walls[i][0],
-      walls[i][1] + walls[i][3],
-      walls[i][0] + walls[i][2],
-      walls[i][1],
+      walls[i].rect[0],
+      walls[i].rect[1] + walls[i].rect[3],
+      walls[i].rect[0] + walls[i].rect[2],
+      walls[i].rect[1],
     )) { flag = true; break; }
   }
   return flag;
@@ -323,23 +317,33 @@ function render() {
 
   // walls
   for (const wall_data of walls) {
-    switch (wall_data[4]) {
+    // animation
+    var animation_offset = Vector2.zero;
+    if (wall_data.animation) { animation_offset = wall_data.animation(game_frame); }
+
+    // sparete with material
+    switch (wall_data["material"]) {
       case "grass":
-        render_rect(wall_data,"#66482b",camera);
-        render_rect([wall_data[0], wall_data[1] + wall_data[3] - 20, wall_data[2], 20],"#3d944f", camera);
-        render_rect([wall_data[0], wall_data[1] + wall_data[3] - 10, wall_data[2], 10],"#49b861", camera);
+        render_rect(wall_data.rect, "#66482b", camera, animation_offset);
+        render_rect([wall_data.rect[0], wall_data.rect[1] + wall_data.rect[3] - 20, wall_data.rect[2], 20], "#3d944f", camera, animation_offset);
+        render_rect([wall_data.rect[0], wall_data.rect[1] + wall_data.rect[3] - 10, wall_data.rect[2], 10], "#49b861", camera, animation_offset);
         break;
       case "dirt":
-        render_rect(wall_data,"#66482b",camera);
+        render_rect(wall_data.rect, "#66482b", camera, animation_offset);
         break;
     }
-    
+
   }
 }
 
-function render_rect(rect_data,color,camera_object) {
+function render_rect(rect_data, color, camera_object, offset = Vector2.zero) {
   ctx.fillStyle = color;
-  ctx.fillRect(rect_data[0] - camera_object.pos.x + camera_object.offset.x, rect_data[1] - camera_object.pos.y + camera_object.offset.y, rect_data[2], rect_data[3]);
+  ctx.fillRect(
+    rect_data[0] - camera_object.pos.x + camera_object.offset.x + offset.x,
+    rect_data[1] - camera_object.pos.y + camera_object.offset.y + offset.y,
+    rect_data[2],
+    rect_data[3]
+  );
 }
 
 // tick
